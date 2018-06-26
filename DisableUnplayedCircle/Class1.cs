76cf1e04626953bf8461bbaa7a157219de682820
☺ <amaury.vanoorenberghe@laposte.net>
2018-06-26 23:42:25 +0200

    Add project files.

---
+++ DisableUnplayedCircle/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisableUnplayedCircle
+{
+    public class Class1
+    {
+    }
+}
