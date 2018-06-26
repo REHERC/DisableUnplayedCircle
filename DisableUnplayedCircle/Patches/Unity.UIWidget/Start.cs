7b18e5c65667d3988fb2313d8a493db42bef883a
☺ <amaury.vanoorenberghe@laposte.net>
2018-06-26 23:48:51 +0200

    Removing the orange "unplayed circle"

---
+++ DisableUnplayedCircle/Patches/Unity.UIWidget/Start.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using Spectrum.API;
+using Spectrum.API.Configuration;
+using Spectrum.API.Interfaces.Plugins;
+using Spectrum.API.Interfaces.Systems;
+using Spectrum.API.Logging;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Distance.Translator
+{
+    public partial class Photon
+    {
+        [HarmonyPatch(typeof(UIExButton), "Start")]
+        internal class UIExButton_Start_Patch : UIExButton
+        {
+            public static void Postfix(UIExButton __instance)
+            {
+                foreach (Transform childTransform in __instance.gameObject.transform.GetChildren())
+                {
+                    GameObject child = childTransform.gameObject;
+                    if (child.name == "Unplayed Circle")
+                    {
+                        child.Destroy();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DisableUnplayedCircle/Storage/CurrentPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectrum.API.Configuration;
+using Spectrum.API.Logging;
+
+namespace Distance.Translator
+{
+    public static class CurrentPlugin
+    {
+        public static void initialize()
+        {
+            Log = new Logger("DisableUnplayedCircle.log");
+            Log.WriteToConsole = true;
+        }
+
+        public static Logger Log;
+    }
+}
